62c12a836d1497e3443e297ead3988117734bb66
lironshani <liron05481@gmail.com>
2022-01-13 18:36:09 +0200

    Adding cool clog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+console.log("cool");
 function App() {
   return (
     <div className="App">
