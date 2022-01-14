368ed7c48bf68a7b0d33701faa2b6704193c60a4
lironshani <liron05481@gmail.com>
2022-01-14 09:37:59 +0200

    deleting link to about us from welcome page

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -5,11 +5,10 @@ import "./Welcome.css";
 
 function Welcome() {
   return (
-    <div >
+    <div>
       Welcome
       <Link to="/sign-up">Sign Up</Link>
       <Link to="/sign-in">Sign In</Link>
-      <Link to="/about-us">About Us</Link>
     </div>
   );
 }
