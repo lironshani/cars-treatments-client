82be82278eeab46c4a207d7034abccad4f53ef04
Evy <devyasiv@gmail.com>
2022-01-16 12:50:16 +0200

    Adding server url to env

--- src/api.js
+++ src/api.js
@@ -1,36 +1,47 @@
-
+import store from "./redux/store";
 
 export const addTreatment = (newTreatment) => {
   newTreatment.date = new Date(newTreatment.date).toISOString();
 
-  return fetch("http://localhost:5000/treatments", {
+  return fetch(`${process.env.REACT_APP_SERVER_URL}/treatments`, {
     method: "POST",
     mode: "cors",
     headers: {
       "Content-Type": "application/json",
-      // authorization: `Bearer ${token}`,
+      authorization: `Bearer ${store.getState().token}`,
     },
     body: JSON.stringify(newTreatment),
   });
 };
 
 export const getAllTreatments = (searchInput) =>
-  fetch("http://localhost:5000/treatments", {
+  fetch(`${process.env.REACT_APP_SERVER_URL}/treatments`, {
     method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${store.getState().token}`,
+    },
   });
 
 export const updateTreatment = (id, newTreatment) => {
   newTreatment.date = new Date(newTreatment.date).toISOString();
 
-  return fetch(`http://localhost:5000/treatments/${id}`, {
+  return fetch(`${process.env.REACT_APP_SERVER_URL}/treatments/${id}`, {
     method: "PUT",
     mode: "cors",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${store.getState().token}`,
+    },
     body: JSON.stringify(newTreatment),
   });
 };
 
 export const deleteTreatment = (id) =>
-  fetch(`http://localhost:5000/treatments/${id}`, {
+  fetch(`${process.env.REACT_APP_SERVER_URL}/treatments/${id}`, {
     method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${store.getState().token}`,
+    },
   });
