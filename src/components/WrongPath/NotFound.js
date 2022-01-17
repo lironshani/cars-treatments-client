cddae9a48d9afa35f546dd34ead280a5d09dcb24
AVIV\ASUS <kamaryaviv@gmail.com>
2022-01-17 11:18:58 +0200

    changes

---
+++ src/components/WrongPath/NotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+const NotFound = () => (
+    <div class="container">
+        <div class="div-dois">
+        <h1 class="title">404</h1>
+        <h2 class="page-not-found" >Page not found</h2>
+        <p class="sorry-txt">Sorry... we can't find the page you're looking for </p>
+        <button class="btn">Go back home</button>
+        </div>
+    </div>
+);
+
+export default NotFound;
