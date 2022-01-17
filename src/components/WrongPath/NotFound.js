3fe203bb4b63881e93dc117a226d066e4c5856e4
AVIV\ASUS <kamaryaviv@gmail.com>
2022-01-17 11:40:20 +0200

    anotherrr

--- src/components/WrongPath/NotFound.js
+++ src/components/WrongPath/NotFound.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./style.css";
 
 const NotFound = () => (
-    <div class="container">
-        <div class="div-dois">
-        <h1 class="title">404</h1>
-        <h2 class="page-not-found" >Page not found</h2>
-        <p class="sorry-txt">Sorry... we can't find the page you're looking for </p>
-        <button class="btn">Go back home</button>
+    <div className="container">
+        <div className="div-dois">
+        <h1 className="title">404</h1>
+        <h2 className="page-not-found" >Page not found</h2>
+        <p className="sorry-txt">Sorry... we can't find the page you're looking for </p>
+        <button className="btn">Go back home</button>
         </div>
     </div>
 );
