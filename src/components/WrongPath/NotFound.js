0978d9e7eb778784b2484ac40aeff4b4a5f76d6e
lironshani <liron05481@gmail.com>
2022-01-17 16:06:51 +0200

    fixing css

--- src/components/WrongPath/NotFound.js
+++ src/components/WrongPath/NotFound.js
@@ -2,14 +2,28 @@ import React from "react";
 import "./style.css";
 
 const NotFound = () => (
-    <div className="container">
-        <div className="div-dois">
-        <h1 className="title">404</h1>
-        <h2 className="page-not-found" >Page not found</h2>
-        <p className="sorry-txt">Sorry... we can't find the page you're looking for </p>
-        <button className="btn">Go back home</button>
-        </div>
+  <div className="container">
+    <div className="div-dois">
+      <h1 className="title">404</h1>
+      <h2 className="page-not-found">Page not found</h2>
+      <p className="sorry-txt">
+        Sorry... we can't find the page you're looking for{" "}
+      </p>
+      <meta name="viewport" content="width=device-width,initial-scale=0.72" />
+
+      <button
+        className="wrong-container btn"
+        type="button"
+        onClick={(e) => {
+          e.preventDefault();
+          window.location.href =
+            "https://cars-treatments-client.herokuapp.com/";
+        }}
+      >
+        Home Page
+      </button>
     </div>
+  </div>
 );
 
 export default NotFound;
